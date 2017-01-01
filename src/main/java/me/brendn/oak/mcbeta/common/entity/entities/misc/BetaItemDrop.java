3d3d1f85bbc7df2f1ae1697b15f52f46b75dba93
brendn <brendn@users.noreply.github.com>
2016-12-31 20:47:32 -0500

    EntityItem implementation

---
+++ src/main/java/me/brendn/oak/mcbeta/common/entity/entities/misc/BetaItemDrop.java
@@ -0,0 +1,38 @@
+package me.brendn.oak.mcbeta.common.entity.entities.misc;
+
+import me.brendn.oak.api.common.entity.entities.misc.ItemDrop;
+import me.brendn.oak.mcbeta.common.entity.BetaEntity;
+import net.minecraft.src.Entity;
+import net.minecraft.src.EntityItem;
+
+public class BetaItemDrop extends BetaEntity implements ItemDrop {
+
+	public BetaItemDrop(Entity reference) {
+		super(reference);
+	}
+
+	@Override
+	public int getPickupDelay() {
+		return getReference().delayBeforeCanPickup;
+	}
+
+	@Override
+	public void setPickupDelay(int delay) {
+		getReference().delayBeforeCanPickup = delay;
+	}
+
+	@Override
+	public int getAge() {
+		return getReference().age;
+	}
+
+	@Override
+	public void setAge(int age) {
+		getReference().age = age;
+	}
+
+	@Override
+	public EntityItem getReference() {
+		return (EntityItem) super.getReference();
+	}
+}
