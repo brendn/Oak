c8b9f4cf74248ed1ccdfb70499cf494832bb2c33
brendn <brendn@users.noreply.github.com>
2017-01-30 20:30:58 -0500

    Hello Kotlin, my old friend!

--- src/main/kotlin/me/brendn/oak/api/client/gui/TextRenderer.kt
+++ src/main/kotlin/me/brendn/oak/api/client/gui/TextRenderer.kt
@@ -1,33 +1,29 @@
-package me.brendn.oak.api.client.gui;
+package me.brendn.oak.api.client.gui
 
-/**
- * Used for rendering text on the screen.
- */
-public interface TextRenderer {
+interface TextRenderer {
 
 	/**
 	 * Draws the given text at the specified position.
-	 *
+
 	 * @param text The text to draw.
 	 * @param x The x-position of the text.
 	 * @param y The y-position of the text.
 	 * @param color The color of the text.
 	 */
-	void drawText(String text, int x, int y, int color);
+	fun drawText(text: String, x: Int, y: Int, color: Int)
 
 	/**
 	 * Draws the given text at the specified position with a shadow.
-	 *
+
 	 * @param text The text to draw.
 	 * @param x The x-position of the text.
 	 * @param y The y-position of the text.
 	 * @param color The color of the text.
 	 */
-	void drawTextWithShadow(String text, int x, int y, int color);
+	fun drawTextWithShadow(text: String, x: Int, y: Int, color: Int)
 
 	/**
-	 * @return The display width of the given {@code text}
+	 * @return The display width of the given `text`
 	 */
-	int getWidth(String text);
-
+	fun getWidth(text: String): Int
 }
