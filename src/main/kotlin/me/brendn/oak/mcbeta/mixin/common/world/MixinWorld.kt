3a3a8e3f8ddfed2c1902901e52635d6803bb081a
brendn <brendn@users.noreply.github.com>
2017-02-02 18:49:53 -0500

    World mixin cleanup

--- src/main/kotlin/me/brendn/oak/mcbeta/mixin/common/world/MixinWorld.kt
+++ src/main/kotlin/me/brendn/oak/mcbeta/mixin/common/world/MixinWorld.kt
@@ -1,7 +1,6 @@
 package me.brendn.oak.mcbeta.mixin.common.world
 
 import com.google.common.collect.ImmutableList
-import com.google.common.collect.Lists
 import me.brendn.oak.api.common.entity.Entity
 import me.brendn.oak.api.common.util.Position
 import me.brendn.oak.api.common.world.World
@@ -15,57 +14,15 @@ import org.spongepowered.asm.mixin.Shadow
 @Mixin(net.minecraft.src.World::class)
 abstract class MixinWorld : World {
 
-	@Shadow abstract fun getBlockId(i: Int, j: Int, k: Int): Int
+	override fun getBlockID(pos: Position) = getBlockId(pos.x, pos.y, pos.z)
+	override fun getBlockMetadata(pos: Position) = getBlockMetadata(pos.x, pos.y, pos.z)
+	override fun isBlockOpaque(pos: Position) = isBlockOpaqueCube(pos.x, pos.y, pos.z)
+	override fun isBlockNormal(pos: Position) = isBlockNormalCube(pos.x, pos.y, pos.z)
+	override fun getBrightness(pos: Position, lightValue: Int) = getBrightness(pos.x, pos.y, pos.z, lightValue)
+	override fun getLightBrightness(pos: Position) = getLightBrightness(pos.x, pos.y, pos.z)
 
-	@Shadow abstract fun getBlockMetadata(i: Int, j: Int, k: Int): Int
-
-	@Shadow abstract fun isBlockNormalCube(i: Int, j: Int, k: Int): Boolean
-
-	@Shadow abstract fun isBlockOpaqueCube(i: Int, j: Int, k: Int): Boolean
-
-	@Shadow abstract fun getBrightness(i: Int, j: Int, k: Int, l: Int): Float
-
-	@Shadow abstract fun getLightBrightness(i: Int, j: Int, k: Int): Float
-
-	@Shadow abstract fun getWorldChunkManager(): WorldChunkManager
-
-	@get:Shadow abstract val randomSeed: Long
-
-	@get:Shadow abstract val worldInfo: WorldInfo
-
-	@Shadow var loadedEntityList: List<net.minecraft.src.Entity>? = null
-
-	override fun getBlockID(pos: Position): Int {
-		return getBlockId(pos.x, pos.y, pos.z)
-	}
-
-	override fun getBlockMetadata(pos: Position): Int {
-		return getBlockMetadata(pos.x, pos.y, pos.z)
-	}
-
-	override fun isBlockOpaque(pos: Position): Boolean {
-		return isBlockOpaqueCube(pos.x, pos.y, pos.z)
-	}
-
-	override fun isBlockNormal(pos: Position): Boolean {
-		return isBlockNormalCube(pos.x, pos.y, pos.z)
-	}
-
-	override fun getBrightness(pos: Position, lightValue: Int): Float {
-		return getBrightness(pos.x, pos.y, pos.z, lightValue)
-	}
-
-	override fun getLightBrightness(pos: Position): Float {
-		return getLightBrightness(pos.x, pos.y, pos.z)
-	}
-
-	override fun getSeed(): Long {
-		return randomSeed
-	}
-
-	override fun isRaining(): Boolean {
-		return worldInfo.raining
-	}
+	override fun getSeed() = randomSeed
+	override fun isRaining() = worldInfo.raining
 
 	override fun getBiome(chunkX: Int, chunkZ: Int): Biome {
 		return getWorldChunkManager().getBiomeGenAt(chunkX, chunkZ) as Biome
@@ -74,4 +31,16 @@ abstract class MixinWorld : World {
 	override fun getLoadedEntities(): Collection<Entity> {
 		return ImmutableList.copyOf((this.loadedEntityList as Collection<Entity>))
 	}
+
+	@Shadow abstract fun getBlockId(i: Int, j: Int, k: Int): Int
+	@Shadow abstract fun getBlockMetadata(i: Int, j: Int, k: Int): Int
+	@Shadow abstract fun isBlockNormalCube(i: Int, j: Int, k: Int): Boolean
+	@Shadow abstract fun isBlockOpaqueCube(i: Int, j: Int, k: Int): Boolean
+	@Shadow abstract fun getBrightness(i: Int, j: Int, k: Int, l: Int): Float
+	@Shadow abstract fun getLightBrightness(i: Int, j: Int, k: Int): Float
+	@Shadow abstract fun getWorldChunkManager(): WorldChunkManager
+
+	@get:Shadow abstract val randomSeed: Long
+	@get:Shadow abstract val worldInfo: WorldInfo
+	@Shadow var loadedEntityList: List<net.minecraft.src.Entity>? = null
 }
